b816ffd57c42a5ca1760057faaf65c0ccd9289d1
jbockerstette <jim.bockerstette@gmail.com>
2018-03-30 10:19:33 -0700

    Passes all tests.

--- src/primality.js
+++ src/primality.js
@@ -4,7 +4,56 @@ process.stdin.setEncoding('ascii');
 let input_stdin = '';
 let input_stdin_array = '';
 let input_currentline = 0;
-const input = [3, 12, 5, 7];
+const input = [
+  45,
+  1,
+  2,
+  3,
+  5,
+  7,
+  11,
+  13,
+  17,
+  19,
+  23,
+  29,
+  31,
+  37,
+  41,
+  43,
+  47,
+  53,
+  59,
+  61,
+  67,
+  71,
+  73,
+  79,
+  83,
+  89,
+  97,
+  101,
+  103,
+  107,
+  109,
+  113,
+  127,
+  131,
+  137,
+  139,
+  149,
+  151,
+  157,
+  163,
+  167,
+  173,
+  179,
+  181,
+  191,
+  193,
+  197,
+  199
+];
 
 process.stdin.on('data', data => {
   input_stdin += data;
@@ -24,12 +73,31 @@ function readLine() {
 }
 
 // ///////////// ignore above this line ////////////////////
+function isPrime(test) {
+  if (test === 1) {
+    return false;
+  }
+  if (test === 2) {
+    return true;
+  }
+  if (test % 2 === 0) {
+    return false;
+  }
+  const stop = Number.parseInt(test / 2, 10);
+
+  for (let i = 3; i < stop; i += 2) {
+    if (test % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function main() {
   const p = parseInt(readLine(), 10);
   for (let a0 = 0; a0 < p; a0++) {
     const n = parseInt(readLine(), 10);
-    console.log(n);
+    console.log(isPrime(n) ? 'Prime' : 'Not prime');
   }
 }
 
