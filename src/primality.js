d5942bf55b50e9612dab763f15e7592ec1b5b385
jbockerstette <jim.bockerstette@gmail.com>
2018-03-30 09:24:15 -0700

    Added some eslint.

--- src/primality.js
+++ src/primality.js
@@ -4,32 +4,33 @@ process.stdin.setEncoding('ascii');
 let input_stdin = '';
 let input_stdin_array = '';
 let input_currentline = 0;
+const input = [3, 12, 5, 7];
 
 process.stdin.on('data', data => {
-  console.log('hi1');
   input_stdin += data;
 });
 
 process.stdin.on('end', () => {
-  console.log('hi2');
   input_stdin_array = input_stdin.split('\n');
   main();
 });
 
 function readLine() {
-  console.log('hi3');
   return input_stdin_array[input_currentline++];
+}
+
+function readLine() {
+  return input[input_currentline++];
 }
 
 // ///////////// ignore above this line ////////////////////
 
 function main() {
-  console.log('hi');
-  const n = parseInt(readLine());
-  const m = parseInt(readLine());
-  const grid = [];
-  for (grid_i = 0; grid_i < n; grid_i++) {
-    grid[grid_i] = readLine().split(' ');
-    grid[grid_i] = grid[grid_i].map(Number);
+  const p = parseInt(readLine(), 10);
+  for (let a0 = 0; a0 < p; a0++) {
+    const n = parseInt(readLine(), 10);
+    console.log(n);
   }
 }
+
+main();
